260a9e5492ad826e1a3c5a1d430fed412263ebb7
C0sm0Black <soulker_mozyr@yahoo.com>
2023-07-26 13:19:49 +0200

    1. Созданы все Entity 2. Созданы DTO для Entity 3. Созданы мапперы для DTO и Entity. 4. Созданы контролеры и сервисы для них. 5. Реализовано для User роли.

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -1,20 +1,25 @@
 package ru.skypro.homework.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import ru.skypro.homework.dto.Role;
+import ru.skypro.homework.service.UserService;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@RequiredArgsConstructor
+
 public class WebSecurityConfig {
+
+    private final UserService userService;
 
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**",
@@ -22,19 +27,23 @@ public class WebSecurityConfig {
             "/v3/api-docs",
             "/webjars/**",
             "/login",
-            "/register"
+            "/register",
+            "/ads",
+            "/images"
+
     };
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user =
-                User.builder()
-                        .username("user@gmail.com")
-                        .password("password")
-                        .passwordEncoder(passwordEncoder::encode)
-                        .roles(Role.USER.name())
-                        .build();
-        return new InMemoryUserDetailsManager(user);
+
+        User.builder()
+                .username("user@gmail.com")
+                .password("password")
+                .passwordEncoder(passwordEncoder::encode)
+                .roles(Role.USER.name())
+                .build();
+        return new InMemoryUserDetailsManager(userService.getAllUserDetails());
+
     }
 
     @Bean
